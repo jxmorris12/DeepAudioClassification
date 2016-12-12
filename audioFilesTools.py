b5a5823cf1ad09ed9758238b9d94ed5d2a4b352c
jxmorris12 <jxmorris12@gmail.com>
2016-12-12 02:20:18 -0500

    cache audio file data

--- audioFilesTools.py
+++ audioFilesTools.py
@@ -4,12 +4,20 @@ import eyed3
 #Remove logs
 eyed3.log.setLevel("ERROR")
 
+loaded_files = {}
+
+# Cache audio file data
+def loadFile(filename):
+  if filename not in loaded_files:
+    loaded_files[filename] eyed3.load(filename)
+  return loaded_files[filename]
+
 def isMono(filename):
-	audiofile = eyed3.load(filename)
+	audiofile = loadFile(filename)
 	return audiofile.info.mode == 'Mono'
 
 def getGenre(filename):
-	audiofile = eyed3.load(filename)
+	audiofile = loadFile(filename)
 	#No genre
 	if not audiofile.tag.genre:
 		return None
